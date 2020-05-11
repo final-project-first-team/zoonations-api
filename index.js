faa9968b1cda82b1afd637e0af179138eb5546e6
targhib <targhib.ibrahim49@gmail.com>
2020-05-11 16:16:07 +0700

    fix bug

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-const express = require("express");
-const app = express()
-const cors = require("cors")
-const { PORT, db } = require("./config")
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const { PORT, db } = require('./config');
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", (req, res) => {
-res.status(200).json({messege:"hello to nosql api"})
+app.get('/', (req, res) => {
+	res.status(200).json({ messege: 'hello to nosql api' });
 });
 
-// app.use("/users", require("./routes/users"));
+app.use('/users', require('./routes/users'));
 // app.use("/appointment", require("./routes/"))
 
-if (db){
-    app.listen(PORT, ()=>{
-        console.log(`this app listen to port ${PORT}`);
-        
-    })
+if (db) {
+	app.listen(PORT, () => {
+		console.log(`this app listen to port ${PORT}`);
+	});
 }
