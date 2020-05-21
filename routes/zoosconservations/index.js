8a33cbb254163a6a1dda1228db2819c9870d9d83
targhib <targhib.ibrahim49@gmail.com>
2020-05-21 23:44:51 +0700

    fix bug

--- routes/zoosconservations/index.js
+++ routes/zoosconservations/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAll, create, edit, deleteById } = require('./controller');
+const { getAll, getById, create, edit, deleteById } = require('./controller');
 
 router.get('/', getAll);
+router.get('/:id', getById);
 router.post('/', create);
 router.put('/:id', edit);
 router.delete('/:id', deleteById);
